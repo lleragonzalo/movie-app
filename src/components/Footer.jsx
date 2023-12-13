064cebc4f24cd3d4110c8efe785c64a4d0a8ee8f
lleragonzalo <llegonza2@gmail.com>
2023-12-12 23:14:11 -0300

    test deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 function Footer() {
   return (
     <div className="footer">
-      <p>⚡Developed by ⚡</p>
+      <p>⚡⚡Developed by </p>
       <a href="https://github.com/lleragonzalo" target="_blank" rel="noopener noreferrer">
       @lleragonzalo
       </a>
+      ⚡⚡
     </div>
   );
 }
