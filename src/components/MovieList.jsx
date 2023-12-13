edb44a4592e1df307f44f337f2e6182faeacd893
lleragonzalo <llegonza2@gmail.com>
2023-12-12 22:54:06 -0300

    hover effect en movie

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,9 +2,116 @@ import Movie from './Movie'
 
 function MovieList() {
     return (
-      <div className="movieList">
-       <Movie />
-      </div>
+    <div className="carousel">
+      <Movie
+        width={200}
+        height={300}
+        movie={{ 
+          "Title": "John Wick",
+          "Year": "2014",
+           "imdbID" : "MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg"
+        }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "John Wick: Chapter 2",
+          "Year": "2017",
+          "imdbID" : "MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_SX300",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_SX300.jpg"
+        }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "John Wick: Chapter 3 - Parabellum",
+          "Year": "2019",
+          "imdbID": "tt6146586",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMDg2YzI0ODctYjliMy00NTU0LTkxODYtYTNkNjQwMzVmOTcxXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg"
+      }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "John Wick: Chapter 4",
+          "Year": "2023",
+          "imdbID": "tt10366206",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_SX300.jpg"  
+        }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "John Wick: Chapter 3 - Parabellum: HBO First Look",
+          "Year": "2019",
+          "imdbID": "tt10275370",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BYmI2NTIzNWYtYjVkNi00ZmRkLTlkYmUtZTU5YjVjNDMzMTdkXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"        }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "The Continental: From the World of John Wick",
+          "Year": "2023",
+          "imdbID": "tt6486762",
+          "Type": "series",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BZDk1MmI3YWEtMGE0MS00ZjFlLWIxMjgtMjc5MmRjOTZiMDc2XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"  
+        }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "John Wick Chapter 2: Wick-vizzed",
+          "Year": "2017",
+          "imdbID": "tt7161870",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BM2I0YWM3Y2EtYzU1YS00MWE1LTg0YjUtNWQ2YTBjZWQ5Mzc1XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"        }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "John Wick: Don't F*#% with John Wick",
+          "Year": "2015",
+          "imdbID": "tt5278630",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ2YzgxNDUtMmM1NS00MzI1LWI1NjYtZjUyYjBhZjQ5MDdmXkEyXkFqcGdeQXVyODA1NjQ0OTY@._V1_SX300.jpg"
+        }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "John Wick: Kill Count",
+          "Year": "2017",
+          "imdbID": "tt7161942",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMjcyM2UzMGQtYzkzYy00MGQ3LWE0MTAtNjIzNzk5MTBhNGQ3XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+        }}
+      />
+      <Movie
+        width={200}
+        height={300}
+        movie={{
+          "Title": "John Wick: The Assassin's Code",
+          "Year": "2015",
+          "imdbID": "tt5278698",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BZmQyNDM5OWEtMTliMS00MDIzLWJlZDAtZDYwNTg2YzJkNzFlXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+        }}
+      />
+    </div>
     );
   }
   
